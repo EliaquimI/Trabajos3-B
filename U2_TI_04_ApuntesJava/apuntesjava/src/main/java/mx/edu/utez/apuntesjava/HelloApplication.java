3966490bb43923c78f04a5cd70a2c47829eab02e
EliaquimI <20243ds038@utez.edu.mx>
2025-06-13 08:56:35 -0600

    Subi la carpeta

--- U2_TI_04_ApuntesJava/apuntesjava/src/main/java/mx/edu/utez/apuntesjava/HelloApplication.java
+++ U2_TI_04_ApuntesJava/apuntesjava/src/main/java/mx/edu/utez/apuntesjava/HelloApplication.java
@@ -1,11 +1,16 @@
 package mx.edu.utez.apuntesjava;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -19,7 +24,21 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         Label label = new Label("Label 1");
-        Label label2 = new Label("Labe2");
+        Label lbname = new Label("Nombre");
+        TextField tfNombre = new TextField();
+        tfNombre.setMaxWidth(200);
+        Label lbapellido = new Label("Apellido");
+        TextField tfApellido = new TextField();
+        tfApellido.setMaxWidth(200);
+
+        Label lbage = new Label("Edad: ");
+        TextField tfage = new TextField();
+        tfage.setMaxWidth(200);
+
+        Label lbRol = new Label("Rol: ");
+        ObservableList<String> rol = FXCollections.observableArrayList("Estudiante","Profesor", "Admin");
+        ComboBox<String> cbRol = new ComboBox(rol);
+
         Button button = new Button("Button Aceptado");
         Button button2 = new Button("Button Cancelado");
 
@@ -32,9 +51,9 @@ public class HelloApplication extends Application {
             label.setText("Cancelado!");
         });
 
-        VBox vbox = new VBox(20,label, label2, button, button2);
-        vbox.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(vbox, 500, 240);
+        VBox vbox = new VBox(5,lbname,tfNombre,lbapellido,tfApellido,lbage,tfage,lbRol,cbRol, button, button2);
+        vbox.setAlignment(Pos.BASELINE_LEFT);
+        Scene scene = new Scene(vbox, 500, 540);
 
         stage.setTitle("Hello!");
 
