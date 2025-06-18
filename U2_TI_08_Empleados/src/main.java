4d1e38a9b9950c5a4fb2d774ac2fb2201a23f84c
EliaquimI <20243ds038@utez.edu.mx>
2025-06-17 21:32:20 -0600

    Subida de carpeta ejercicio 8

--- U2_TI_08_Empleados/src/main.java
+++ U2_TI_08_Empleados/src/main.java
@@ -4,7 +4,7 @@ public class main {
         
         EmpleadoTiempoCompleto empleadoTiempoCompleto = new EmpleadoTiempoCompleto("1", "JUAN", 1020, "Seguro popular");
         EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras("2", "Eliaquim", 10200, 89);
-System.out.println("E-------MPLEADOS----------------");
+System.out.println("-------EMPLEADOS----------------");
         empleadoPorHoras.calcularSalario();
         empleadoTiempoCompleto.calcularSalario();
 
